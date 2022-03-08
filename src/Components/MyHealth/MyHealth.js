7b8aa5fe4e48ca7cacb8b6b4ac7147a0572a1bd4
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-08 13:32:02 -0500

    display the profile list

--- src/Components/MyHealth/MyHealth.js
+++ src/Components/MyHealth/MyHealth.js
@@ -1,20 +1,39 @@
 
-import React from 'react';
-import { useState} from "react";
+import React, { useEffect ,useState} from 'react';
+
 
 export default function MyHealth() {
     const [user, setUser] = useState([]);
 
 
-    const getUser = () => {
-        fetch("http://localhost:3000/person/")
-        .then((response) => response.json())
-        .then((data) => setUser(data.person))
-    }
+   useEffect(() =>{
+    fetch("http://localhost:3000/person/")
+    .then((response) => response.json())
+    .then((data) => setUser(data.person))
+   
+
+   }, []);
+        
+    
+    const profileList = user.map((user) => (
+      <div>
+ <li key = {user._id}>
+            {user.firstName}</li>
+            <li>
+            {user.age}</li>
+            <li>
+            {user.email}
+        </li>
+      </div> 
+    ))
+
 
   return (
       <>
         <div>My Health Page</div>
+        <ul>{profileList}</ul>
+
+       
     </>
   )
 }
