411c361f3239b0adfc50c9a9b002c24522a33947
Anju Dhungana <anjudhungana28@gmail.com>
2022-03-08 14:02:31 -0500

    added a delete button with delete function

--- src/Components/MyHealth/MyHealth.js
+++ src/Components/MyHealth/MyHealth.js
@@ -3,9 +3,10 @@ import React, { useEffect ,useState} from 'react';
 
 
 export default function MyHealth() {
+    // creating use state for user
     const [user, setUser] = useState([]);
 
-
+// fetching data from database api
    useEffect(() =>{
     fetch("http://localhost:3000/person/")
     .then((response) => response.json())
@@ -13,27 +14,43 @@ export default function MyHealth() {
    
 
    }, []);
+
+
+    // delete function to delete user
         
+   const handleDelete = (e)=> {
+    console.log(e.target.value)
+    let id = e.target.value;
+    fetch("http://localhost:3000/person/" + id ,{method:'DELETE'})
+}
+
     
+//    getting user from the database
     const profileList = user.map((user) => (
       <div>
- <li key = {user._id}>
+ <li key = {user._id} >
             {user.firstName}</li>
             <li>
             {user.age}</li>
             <li>
             {user.email}
         </li>
+        <button type='button'value = {user._id} onClick ={handleDelete}>Delete</button>
       </div> 
     ))
+
+
 
 
   return (
       <>
         <div>My Health Page</div>
+        {/* returning profile list of the user */}
         <ul>{profileList}</ul>
 
-       
+
+{/* delete button to remove user */}
+      
     </>
   )
 }
