ae25f1e7b6f8c2af9c0a8336b701b82e2168df18
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-08 13:25:57 -0800

    updated urlBase and added people usestate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import "./App.css";
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Link, Routes, Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import Header from './Components/Header/Header'
 // import Footer from './Components/Footer/Footer'
-// import Home from './Components/Home/Home'
-// import AboutTeam from './Components/AboutTeam/AboutTeam'
-// import MyHealth from './Components/MyHealth/MyHealth'
+import Home from './Components/Home/Home'
+import AboutTeam from './Components/AboutTeam/AboutTeam'
+import MyHealth from './Components/MyHealth/MyHealth'
 // import DailyTracker from './Components/DailyTracker/DailyTracker'
 
 function App() {
 
-  const urlBase = 'localhost:3000/'
+  const urlBase = 'http://localhost:3000'
+  const [people, setPeople] = useState([])
+
+  useEffect(() =>{
+    fetch(urlBase + '/person')
+    .then((response) => response.json())
+    .then((data) => setPeople(data.person))
+   }, []);
 
   return (
     <div className="App">
@@ -18,11 +26,11 @@ function App() {
 
       <main>
         <Routes>
-          {/* <Route path="/" element={<Home urlBase={urlBase}/>} />
+          <Route path="/" element={<Home urlBase={urlBase} people={people} setPeople={setPeople}/>} />
           <Route path="/home" element={<Navigate to="/" />} />
           <Route path="/about-team" element={<AboutTeam />} />
-          <Route path="/daily-tracker" element={<DailyTracker urlBase={urlBase}/>} />
-          <Route path="/my-health" element={<MyHealth urlBase={urlBase}/>} /> */}
+          {/* <Route path="/daily-tracker" element={<DailyTracker urlBase={urlBase} people={people} setPeople={setPeople}/>} /> */}
+          <Route path="/my-health" element={<MyHealth urlBase={urlBase} people={people} setPeople={setPeople}/>} />
         </Routes>
       </main>
 
