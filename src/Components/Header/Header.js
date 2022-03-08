f93254886fd9e5e443156300274281b9ecba4e5d
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-03-08 11:28:12 -0800

    header with links added, minimal css attaached with burger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,1 +1,26 @@
-//header
+import { Link } from "react-router-dom";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="links">
+        <p>
+          <Link to="/">Home</Link>
+        </p>
+        <p>
+          <Link to="/daily-tracker">Daily Tracker</Link>
+        </p>
+        <p>
+          <Link to="/my-health">My Health</Link>
+        </p>
+        <p>
+          <Link to="/about-team">Meet the Team</Link>
+        </p>
+      </div>
+      <img className="menu" src="https://cdn0.iconfinder.com/data/icons/essentials-line/100/Menu-1024.png"></img>
+    </header>
+  );
+};
+
+export default Header;
