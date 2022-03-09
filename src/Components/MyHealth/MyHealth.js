063b05808f1390c657996c28e9425ad0a10690d6
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-08 16:49:03 -0800

    updated my health page to filter by email and rendered activity data from weekly view
    Co-authored-by: Kae Saetern <kae.saetern@live.com>
    Co-authored-by: Lauren Colvin <lcolvin2013@gmail.com>
    Co-authored-by: Anju Dhungana <anjudh5802@gmail.com>

--- src/Components/MyHealth/MyHealth.js
+++ src/Components/MyHealth/MyHealth.js
@@ -1,55 +1,66 @@
+import React, { useEffect, useState } from "react";
+import WeeklyView from "./../WeeklyView/WeeklyView";
 
-import React, { useEffect ,useState} from 'react';
+export default function MyHealth({ urlBase, people, setPeople }) {
+  // creating use state for user
+  const [user, setUser] = useState([]);
+  const [email, setEmail] = useState("");
+  const [personId, setPersonId] = useState("");
 
+// function for setting the email
+  const emailHandleChange = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+    let person = people.filter((n) => n.email === event.target.value);
+    setPersonId(person[0]._id);
+  };
 
-export default function MyHealth({urlBase, people, setPeople}) {
-    // creating use state for user
-    const [user, setUser] = useState([]);
-    
-// fetching data from database api
-   useEffect(() =>{
-    fetch(urlBase + '/person')
-    .then((response) => response.json())
-    .then((data) => setUser(data.person))
-   
-   }, []);
+// function for submitting the email
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+    fetch(urlBase + "/person/" + personId)
+      .then((response) => response.json())
+      .then((data) => setUser(data.person));
+  };
 
-
-
-    // delete function to delete user
-        
-   const handleDelete = (e)=> {
-    console.log(e.target.value)
+  // delete function to delete user
+  const handleDelete = (e) => {
+    console.log(e.target.value);
     let id = e.target.value;
-    fetch(urlBase +'/person/' + id ,{method:'DELETE'})
-}
-
-    
-//    getting user from the database
-    const profileList = user.map((user) => (
-      <div>
- <li key = {user._id} >
-           Name : {user.firstName}</li>
-            <li>
-           Age : {user.age}</li>
-            <li>
-          Email :  {user.email}
-        </li>
-        {/* delete button to remove user */}
-        <button type='button'value = {user._id} onClick ={handleDelete}>Delete</button>
-      </div> 
-    ))
-
-
-
+    fetch(urlBase + "/person/" + id, { method: "DELETE" });
+  };
 
   return (
-      <>
-        <div>My Health Page</div>
-        {/* returning profile list of the user */}
-        <ul>{profileList}</ul>
-        
-  
+    <>
+      <h3>My Health Page</h3>
+      {/* returning profile list of the user */}
+      <form onSubmit={handleSubmit}>
+        <div className="email">
+          <input
+            onChange={emailHandleChange}
+            name="email"
+            placeholder="Email"
+          ></input>
+        </div>
+        <input className="submit-button" type="submit"></input>
+      </form>
+      {user.length === 0 ? (
+        <div></div>
+      ) : (
+        <div>
+        <ul>
+          {" "}
+          <li key={user._id}>Name : {user.firstName}</li>
+          <li>Age : {user.age}</li>
+          <li>Email : {user.email}</li>
+          <button type="button" value={user._id} onClick={handleDelete}>
+            Delete
+          </button>
+        </ul>
+        <WeeklyView urlBase={urlBase} user={user} />
+        </div>
+      )}
     </>
-  )
+  );
 }
--- src/Components/WeeklyView/WeeklyView.js
+++ src/Components/WeeklyView/WeeklyView.js
@@ -1,1 +1,21 @@
 //weeklyview
+
+import { useEffect } from "react";
+
+const WeeklyView = ({ urlBase, user }) => {
+  const activities = user.activity;
+    console.log(user)
+  const list = activities.map((activity) => {
+    return (
+      <div>
+          <h3>Weekly Activity</h3>
+        <p>Exercise: {activity.exercise}</p>
+        <p>Intensity: {activity.intensity}</p>
+      </div>
+    );
+  });
+
+  return <div>{list}</div>;
+};
+
+export default WeeklyView;
