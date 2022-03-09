ee9a3776d8ceb658cfcbc566dcc29ad34e5ca504
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-03-09 09:54:01 -0800

    add person post,
    Co-authored-by: Lauren Colvin <lcolvin2013@gmail.com>
    Co-authored-by: Anju Dhungana <anjudh5802@gmail.com>
    
    Co-authored-by: Rebecca Neimeyer <rebeccaneimeyer@gmail.com>

--- src/Components/DailyTracker/DailyTracker.js
+++ src/Components/DailyTracker/DailyTracker.js
@@ -1,140 +1,238 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-const DailyTracker = ({urlBase, people, setPeople}) => {
+const DailyTracker = ({ urlBase, people, setPeople }) => {
+  const [email, setEmail] = useState("");
+  const [personId, setPersonId] = useState("");
+  const [date, setDate] = useState("");
+  const [exercise, setExercise] = useState("");
+  const [length, setLength] = useState("");
+  const [intensity, setIntensity] = useState("");
+  const [activityId, setActivityId] = useState("");
+  const [user, setUser] = useState({});
+  const [allActivities, setAllActivities] = useState([]);
 
-    const [email, setEmail] = useState("")
-    const [personId, setPersonId] = useState("")
-    const [date, setDate] = useState("")
-    const [exercise, setExercise] = useState("")
-    const [length, setLength] = useState("")
-    const [intensity, setIntensity] = useState("")
-    const [activityId, setActivityId] = useState("")
+  const emailHandleChange = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+    let person = people.filter((n) => n.email === event.target.value);
+    setPersonId(person[0]._id);
+    setUser(person[0]);
+    setAllActivities(person[0].activity);
+  };
 
+  //see if we need later
+  const putActivity = (activity) => {
+    console.log(personId);
+    console.log(allActivities)
+    const activitiesCopy = [...allActivities];
+    console.log(...allActivities)
+    activitiesCopy.push(activity)
+    let data = {
+      activity: activitiesCopy,
+    };
+    let options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(urlBase + "/person/" + personId, options)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
-    const emailHandleChange = (event) => {
-        event.preventDefault();
-        setEmail(event.target.value);
-        let person = people.filter((n) => n.email === event.target.value)
-        setPersonId(person[0]._id)
-      };
-      
-    const putActivity = (id) => {
-        console.log(personId)
-        let data = {
-            "activity": id
-            }
-            let options = {
-                method: 'PUT',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            }
-        fetch(urlBase + '/person/' + personId, options)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-       }
+  const postActivity = (id) => {
+    console.log(personId);
+    let data = {
+      activity: id,
+    };
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(urlBase + "/person/" + personId, options)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
-      const dateHandleChange = (event) => {
-        event.preventDefault();
-        setDate(event.target.value);
-      };
+  const dateHandleChange = (event) => {
+    event.preventDefault();
+    setDate(event.target.value);
+  };
 
-      const activityHandleChange = (event) => {
-        event.preventDefault();
-        setExercise(event.target.value);
-      };
+  const activityHandleChange = (event) => {
+    event.preventDefault();
+    setExercise(event.target.value);
+  };
 
-      const lengthHandleChange = (event) => {
-        event.preventDefault();
-        setLength(event.target.value);
-      };
+  const lengthHandleChange = (event) => {
+    event.preventDefault();
+    setLength(event.target.value);
+  };
 
-      const intensityHandleChange = (event) => {
-        event.preventDefault();
-        setIntensity(event.target.value);
-      };
+  const intensityHandleChange = (event) => {
+    event.preventDefault();
+    setIntensity(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        let data = {
-            "date": date,
-            "exercise": exercise,
-            "length": length,
-            "intensity": intensity,
-        }
-        let options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-          }
-        fetch(`${urlBase}/activity`, options)
-            .then(res => res.json())
-            .then(data => putActivity(data.activity._id))
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = {
+      date: date,
+      exercise: exercise,
+      length: length,
+      intensity: intensity,
+    };
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(`${urlBase}/person/${personId}`)
+      .then((res) => res.json())
+      .then((data) => setAllActivities(data.activity));
+    fetch(`${urlBase}/activity`, options)
+      .then((res) => res.json())
+      .then((data) => putActivity(data));
+  };
 
-      }
-
-    return (
-        <div>
-            <h1> Welcome to the Daily Tracker</h1>
-            <h2>Please input your email and the date to track your activity today!</h2>
-            <form onSubmit={handleSubmit} >
-                <div className="email">
-                    <input onChange={emailHandleChange} name="email" placeholder="Email"></input>
-                </div>
-                <br />
-                <div className="date">
-                    <input onChange={dateHandleChange} name="date" placeholder="Date"></input>
-                </div>
-                <br />
-                <div className="activity">
-                    <label> What activity did you do? </label>
-                    <select onChange={activityHandleChange}  className="dropdown" id="activity-type">
-                        <option className='option' name="exercise" value=""></option>
-                        <option className='option' name="exercise" value="walk">Walk</option>
-                        <option className='option' name="exercise" value="run">Run</option>
-                        <option className='option' name="exercise" value="bike">Bike</option>
-                        <option className='option' name="exercise" value="swim">Swim</option>
-                        <option className='option' name="exercise" value="hike">Hike</option>
-                        <option className='option' name="exercise" value="yoga">Yoga</option>
-                        <option className='option' name="exercise" value="pilates">Pilates</option>
-                        <option className='option' name="exercise" value="hiit">HIIT</option>
-                        <option className='option' name="exercise" value="sports">Sports</option>
-                        <option className='option' name="exercise" value="dance">Dance</option>
-                        <option className='option' name="exercise" value="core">Core</option>
-                        <option className='option' name="exercise" value="arms">Arms</option>
-                        <option className='option' name="exercise" value="legs">Legs</option>
-                        <option className='option' name="exercise" value="back">Back</option>
-                        <option className='option' name="exercise" value="full-body-strength">Full Body Strength</option>
-                    </select>
-                </div>
-                <div onChange={lengthHandleChange} className="length">
-                    <input name="length" placeholder="Length in minutes"></input>
-                </div>
-                <br />
-                <div className="intensity">
-                    <label> How intense was your activity? </label>
-                    <select onChange={intensityHandleChange} className="dropdown" id="activity-type">
-                        <option className='option' name="intensity" value=""></option>
-                        <option className='option' name="intensity" value="1">1</option>
-                        <option className='option' name="intensity" value="2">2</option>
-                        <option className='option' name="intensity" value="3">3</option>
-                        <option className='option' name="intensity" value="4">4</option>
-                        <option className='option' name="intensity" value="5">5</option>
-                        <option className='option' name="intensity" value="6">6</option>
-                        <option className='option' name="intensity" value="7">7</option>
-                        <option className='option' name="intensity" value="8">8</option>
-                        <option className='option' name="intensity" value="9">9</option>
-                        <option className='option' name="intensity" value="10">10</option>
-                    </select>
-                </div>
-                <br />
-                <input className="submit-button" type="submit"></input>
-            </form>
+  return (
+    <div>
+      <h1> Welcome to the Daily Tracker</h1>
+      <h2>
+        Please input your email and the date to track your activity today!
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="email">
+          <input
+            onChange={emailHandleChange}
+            name="email"
+            placeholder="Email"
+          ></input>
         </div>
-    )
-}
+        <br />
+        <div className="date">
+          <input
+            onChange={dateHandleChange}
+            name="date"
+            placeholder="Date"
+          ></input>
+        </div>
+        <br />
+        <div className="activity">
+          <label> What activity did you do? </label>
+          <select
+            onChange={activityHandleChange}
+            className="dropdown"
+            id="activity-type"
+          >
+            <option className="option" name="exercise" value=""></option>
+            <option className="option" name="exercise" value="walk">
+              Walk
+            </option>
+            <option className="option" name="exercise" value="run">
+              Run
+            </option>
+            <option className="option" name="exercise" value="bike">
+              Bike
+            </option>
+            <option className="option" name="exercise" value="swim">
+              Swim
+            </option>
+            <option className="option" name="exercise" value="hike">
+              Hike
+            </option>
+            <option className="option" name="exercise" value="yoga">
+              Yoga
+            </option>
+            <option className="option" name="exercise" value="pilates">
+              Pilates
+            </option>
+            <option className="option" name="exercise" value="hiit">
+              HIIT
+            </option>
+            <option className="option" name="exercise" value="sports">
+              Sports
+            </option>
+            <option className="option" name="exercise" value="dance">
+              Dance
+            </option>
+            <option className="option" name="exercise" value="core">
+              Core
+            </option>
+            <option className="option" name="exercise" value="arms">
+              Arms
+            </option>
+            <option className="option" name="exercise" value="legs">
+              Legs
+            </option>
+            <option className="option" name="exercise" value="back">
+              Back
+            </option>
+            <option
+              className="option"
+              name="exercise"
+              value="full-body-strength"
+            >
+              Full Body Strength
+            </option>
+          </select>
+        </div>
+        <div onChange={lengthHandleChange} className="length">
+          <input name="length" placeholder="Length in minutes"></input>
+        </div>
+        <br />
+        <div className="intensity">
+          <label> How intense was your activity? </label>
+          <select
+            onChange={intensityHandleChange}
+            className="dropdown"
+            id="activity-type"
+          >
+            <option className="option" name="intensity" value=""></option>
+            <option className="option" name="intensity" value="1">
+              1
+            </option>
+            <option className="option" name="intensity" value="2">
+              2
+            </option>
+            <option className="option" name="intensity" value="3">
+              3
+            </option>
+            <option className="option" name="intensity" value="4">
+              4
+            </option>
+            <option className="option" name="intensity" value="5">
+              5
+            </option>
+            <option className="option" name="intensity" value="6">
+              6
+            </option>
+            <option className="option" name="intensity" value="7">
+              7
+            </option>
+            <option className="option" name="intensity" value="8">
+              8
+            </option>
+            <option className="option" name="intensity" value="9">
+              9
+            </option>
+            <option className="option" name="intensity" value="10">
+              10
+            </option>
+          </select>
+        </div>
+        <br />
+        <input className="submit-button" type="submit"></input>
+      </form>
+    </div>
+  );
+};
 
 export default DailyTracker;
--- src/Components/WeeklyView/WeeklyView.js
+++ src/Components/WeeklyView/WeeklyView.js
@@ -4,11 +4,12 @@ import { useEffect } from "react";
 
 const WeeklyView = ({ urlBase, user }) => {
   const activities = user.activity;
-    console.log(user)
+  console.log(user);
   const list = activities.map((activity) => {
     return (
       <div>
-          <h3>Weekly Activity</h3>
+        <h3>Weekly Activity</h3>
+        <p>Date: {activity.date}</p>
         <p>Exercise: {activity.exercise}</p>
         <p>Intensity: {activity.intensity}</p>
       </div>
