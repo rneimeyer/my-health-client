be2453ee785112d7c5e5fe675bd9bcd43d765c21
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-03-10 11:25:41 -0800

    footer added, grids added to multiple pages

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,1 +1,19 @@
 //footer
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="site-footer">
+        <div class="container">
+          <p class="copyright-text">
+            Copyright &copy; 2022 All Rights Reserved by {" "}
+            <span>LARK</span>.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
