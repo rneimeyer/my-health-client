312306480f1ade668ae14e48119d65de7dc696bc
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-04-11 19:23:35 -0700

    updated new deployed api url '

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -4,7 +4,7 @@ let apiUrl;
 if (window.location.hostname === "localhost") {
   apiUrl = "http://localhost:4000";
 } else {
-  apiUrl = "https://agile-temple-34348.herokuapp.com";
+  apiUrl = "https://peaceful-beyond-24399.herokuapp.com";
 }
 
 // export api url
