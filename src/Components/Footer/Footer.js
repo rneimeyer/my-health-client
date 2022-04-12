a6302bfc2d5d361f03c2aef220396470bfcf2d31
Rebecca Neimeyer <rebeccaneimeyer@Rebeccas-MacBook.local>
2022-04-11 19:00:22 -0700

    removed unused divs

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,13 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <footer className="site-footer">
-        <div className="container">
-          <p className="copyright-text">
-            Copyright &copy; 2022 All Rights Reserved by <span>LARK</span>🐣
-          </p>
-        </div>
-      </footer>
+      <p className="copyright-text">
+        Copyright &copy; 2022 All Rights Reserved by <span>LARK</span>🐣
+      </p>
     </div>
   );
 };
